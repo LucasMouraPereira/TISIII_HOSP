8c65b59298c8293a00b2a31f7332b0f36b4179b5
Lucaslimas95 <lukaslimas95@outlook.com>
2020-04-28 16:55:59 -0300

    Add files via upload

--- src/database/database.js
+++ src/database/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require("sequelize");
 
-const connection = new Sequelize('hosp', 'root', 'Lucas24056401', {
+const connection = new Sequelize('hosp', 'root', '123456', {
     host: 'localhost',
     dialect: 'mysql',
     timezone: "-03:00"
