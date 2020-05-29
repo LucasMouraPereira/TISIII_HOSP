cb52d6d79f61c75aecc93f8d2df8ff871b36e05c
Lucas Moura Pereira <44249663+LucasMouraPereira@users.noreply.github.com>
2020-05-29 14:21:11 -0300

    Sprint4-send email

--- src/app.js
+++ src/app.js
@@ -2,10 +2,14 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const connection = require("./database/database");
+const {nome, email, data_retorno, status} = require("./model/record");
+const mail = require("./infra/sendMail");
+
 
 //rotas
 const records = require("./router/recordRouter");
 const filter = require("./router/filterRouter");
+const file = require("./router/exportRouter");
 
 //View engine
 app.set('view engine', 'ejs');
@@ -27,9 +31,16 @@ connection
         console.log(error);
     });
 
+app.use('/uploads', express.static(process.cwd() + '/uploads'))
+app.use('/exports', express.static(process.cwd() + '/exports'))
+
 app.use("/", records);
-app.use("/filter", filter);
+app.use("/", filter);
+app.use("/", file);
+
+mail.sendMailRemember(email, nome, data_retorno);
+mail.sendMailNotAttend(email, nome, data_retorno, status);
 
 app.listen(8080, () => {
-    console.log("O servidor esta rodando");
+    console.log("Server running on localhost:8080");
 });
